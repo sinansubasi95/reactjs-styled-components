290f3ba21a14c91dafa1cd01473451efa2f12d89
sinansubasi95 <sinan95@gmail.com>
2021-01-04 13:33:53 +0300

    InfoSection and HomePage have been added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import GlobalStyle from './globalStyles';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/HomePage/Home';
 import { Navbar } from './components';
 
 function App() {
@@ -8,6 +9,9 @@ function App() {
         <Router>
             <GlobalStyle />
             <Navbar />
+            <Switch>
+                <Route path="/" exact component={Home} />
+            </Switch>
         </Router>
     );
 }
